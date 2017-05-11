9a94fe427d05de2600611109686e5caf7a767169
Marcel Roesink <mroesink@rocvantwente.nl>
2017-05-11 17:25:48 +0200

    Startup code for tutorial TODOs (Pluralsight)

---
+++ app/src/main/java/nl/rocvantwente/rsk01/thingstodo/CategoryActivity.java
@@ -0,0 +1,13 @@
+package nl.rocvantwente.rsk01.thingstodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
---
+++ app/src/main/java/nl/rocvantwente/rsk01/thingstodo/TodoActivity.java
@@ -0,0 +1,19 @@
+package nl.rocvantwente.rsk01.thingstodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class TodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+
+        Intent intent = getIntent();
+        String content = intent.getStringExtra("Content");
+        EditText editToDo = (EditText) findViewById(R.id.editTodo);
+    }
+}
